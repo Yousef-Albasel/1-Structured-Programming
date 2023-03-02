f08b8cbc1b8efb94ed47cc9e6ee3029d0a2cdd5a
Blade <122945991+Omar0x0@users.noreply.github.com>
2023-03-01 17:23:06 -0800

    Fixed some issues
    1. Fixed the buffer overflow problem
    2. Simplified the code a little bit
    3. Used less space (less memory usage)

--- Omar's Individual Tasks solutions/Game_5/main.c
+++ Omar's Individual Tasks solutions/Game_5/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <string.h>
 
 void rand_string(char *str, int len);
 void shuffle_characters(char *s);
@@ -11,73 +11,50 @@ char all_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 int main()
 {
-    // Give a random seed each time the program gets executed to the srand() function
+    // Give a random seed each time the program gets executed to the srand()
+    // function
     srand(time(NULL));
     int size = 20, flag = 0;
     int score_1 = 0, score_2 = 0, turn = 1;
-    char tmp[size / 2 + 1], str[size];
+    char tmp[size / 2 + 1], str[size + 1];
 
     char screen[] = "Welcome: 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0\n";
     rand_string(tmp, (size / 2) + 1);
     strcpy(str, tmp);
     shuffle_characters(tmp);
-    strcat(str, tmp);
+    strncat(str, tmp, (size / 2) + 1);
+    str[size] = 0;
     for (int i = 0; i < size; i++)
         printf("%c ", str[i]);
     printf("\n");
     sleep(5);
     // Special string to clear the screen
     printf("\033[2J\033[1;1H");
-    while (flag != size)
+    for (; flag != size; turn = (turn & 1 ? 2 : 1))
     {
         char c1, c2;
-        int pos1, pos2, space1, space2;
-        printf(screen);
-        if (turn & 1)
+        int pos1, pos2;
+        printf("%s", screen);
+        printf("Player#%d-score %d: ", turn, turn & 1 ? score_1 : score_2);
+        scanf("%d %d", &pos1, &pos2);
+        c1 = screen[2 * pos1 + 7];
+        screen[2 * pos1 + 7] = str[pos1 - 1];
+        c2 = screen[2 * pos2 + 7];
+        screen[2 * pos2 + 7] = str[pos2 - 1];
+        printf("%s", screen);
+        sleep(4);
+        // Special string to clear the screen
+        printf("\033[2J\033[1;1H");
+        screen[2 * pos1 + 7] = c1;
+        screen[2 * pos2 + 7] = c2;
+        if (str[pos1 - 1] == str[pos2 - 1] && pos1 != pos2)
         {
-            printf("Player#%d-score %d: ", turn, score_1);
-            scanf("%d %d", &pos1, &pos2);
-            space1 = pos1 - 1, space2 = pos2 - 1;
-            c1 = screen[pos1 + space1 + 8];
-            screen[pos1 + space1 + 8] = str[pos1 - 1];
-            c2 = screen[pos2 + space2 + 8];
-            screen[pos2 + space2 + 8] = str[pos2 - 1];
-            printf(screen);
-            sleep(4);
-            // Special string to clear the screen
-            printf("\033[2J\033[1;1H");
-            screen[pos1 + space1 + 8] = c1;
-            screen[pos2 + space2 + 8] = c2;
-            if (str[pos1 - 1] == str[pos2 - 1] && pos1 != pos2)
-            {
-                score_1++, screen[pos1 + space1 + 8] = screen[pos2 + space2 + 8] = '*';
-                flag += 2;
-            }
-            turn *= 2;
-        }
-        else
-        {
-            printf("Player#%d-score %d: ", turn, score_2);
-            scanf("%d %d", &pos1, &pos2);
-            space1 = pos1 - 1, space2 = pos2 - 1;
-            c1 = screen[pos1 + space1 + 8];
-            screen[pos1 + space1 + 8] = str[pos1 - 1];
-            c2 = screen[pos2 + space2 + 8];
-            screen[pos2 + space2 + 8] = str[pos2 - 1];
-            printf(screen);
-            sleep(4);
-            // Special string to clear the screen
-            printf("\033[2J\033[1;1H");
-            screen[pos1 + space1 + 8] = c1;
-            screen[pos2 + space2 + 8] = c2;
-            if (str[pos1 - 1] == str[pos2 - 1] && pos1 != pos2)
-            {
-                score_1++, screen[pos1 + space1 + 8] = screen[pos2 + space2 + 8] = '*';
-                flag += 2;
-            }
-            turn /= 2;
+            screen[2 * pos1 + 7] = screen[2 * pos2 + 7] = '*';
+            flag += 2;
+            turn & 1 ? score_1++ : score_2++;
         }
     }
+
     if (score_1 > score_2)
         printf("The winner is Player 1\n");
     else if (score_2 > score_1)
